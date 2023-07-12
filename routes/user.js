1d550d939730c8cb0fb7e58db00ae1b9f46a0c53
Devina <44644511+Devina6@users.noreply.github.com>
2023-07-12 19:38:38 +0530

    login route

--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const userController = require('../controllers/user');
 
 router.post('/adduser',userController.postUser);
+router.post('/login',userController.login)
 
 module.exports = router;

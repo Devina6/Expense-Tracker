85a0f40e3cc777a4e994228572a8207721d4db10
Devina <44644511+Devina6@users.noreply.github.com>
2023-07-12 18:34:48 +0530

    Update signup.js

--- client/signup.js
+++ client/signup.js
@@ -7,4 +7,13 @@ async function details(e){
         password:document.getElementById("password").value
     };
     let res = await axios.post('http://localhost:5000/adduser',obj);
+
+    let newdiv = document.createElement("div");
+    newdiv.className = "alert alert-danger";
+    newdiv.role = "alert";
+    let child = document.createElement("p");
+    child.textContent = `${res.data.res}`;
+    newdiv.appendChild(child);
+    document.getElementById("warning").appendChild(newdiv);
+  
 }

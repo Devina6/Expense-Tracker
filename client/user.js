f02c6619defd8f521e2c39db71fe6e8fc3d080c7
Devina <44644511+Devina6@users.noreply.github.com>
2023-07-12 19:37:44 +0530

    Update and rename signup.js to user.js

---
+++ client/user.js
@@ -0,0 +1,46 @@
+async function details(e){
+    e.preventDefault();
+    let obj = {
+        firstName:document.getElementById("firstName").value,
+        lastName:document.getElementById("lastName").value,
+        email:document.getElementById("email").value,
+        password:document.getElementById("password").value
+    };
+    let res = await axios.post('http://localhost:5000/adduser',obj);
+
+    let newdiv = document.createElement("div");
+    newdiv.className = "alert alert-danger";
+    newdiv.role = "alert";
+    let child = document.createElement("p");
+    child.textContent = `${res.data.res}`;
+    newdiv.appendChild(child);
+    let warning = document.getElementById("warning")
+    warning.className = "card card-body";
+    warning.appendChild(newdiv);
+}
+  
+}
+
+async function login(e){
+    e.preventDefault();
+    let obj = {
+        email:document.getElementById("email").value,
+        password:document.getElementById("password").value
+    }
+    let res = await axios.post('http://localhost:5000/login',obj);
+
+    let newdiv = document.createElement("div");
+    if (res.data.pass){
+        newdiv.className = "alert alert-success";
+    }else{
+        newdiv.className = "alert alert-danger";
+    }
+    
+    newdiv.role = "alert";
+    let child = document.createElement("p");
+    child.textContent = `${res.data.res}`;
+    newdiv.appendChild(child);
+    let warning = document.getElementById("warning")
+    warning.className = "card card-body";
+    warning.appendChild(newdiv);
+}

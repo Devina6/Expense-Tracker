70eec7a5511c8611ea3692105fe6d7f0a69becb2
Devina <44644511+Devina6@users.noreply.github.com>
2023-07-13 22:46:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,13 +5,21 @@ const path = require('path');
 const sequelize = require('./util/database');
 const cors = require('cors')
 
+const User = require('./models/user');
+const Expense = require('./models/expense');
+
 const userRoutes = require('./routes/user');
+const expenseRoutes = require('./routes/expense');
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use(cors());
 
 app.use(userRoutes);
+app.use('/expense',expenseRoutes);
+
+User.hasMany(Expense);
+Expense.belongsTo(User);
 
 sequelize
     .sync()

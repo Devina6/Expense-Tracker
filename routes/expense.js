82d474a7dfd843b82b1398fa0fee3a6e6ea42c25
Devina <44644511+Devina6@users.noreply.github.com>
2023-07-14 16:37:36 +0530

    authenticate before controllers

--- routes/expense.js
+++ routes/expense.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const expenseController = require('../controllers/expense');
+const userAuthentication = require('../middleware/auth');
 
 
-router.get('/index/:userId',expenseController.getIndex);
-router.post('/addExpense/:userId',expenseController.postExpense);
-router.get('/delete/:expenseId',expenseController.deleteExpense);
+router.get('/index',userAuthentication.authenticate,expenseController.getIndex);
+router.post('/addExpense',userAuthentication.authenticate,expenseController.postExpense);
+router.get('/delete/:expenseId',userAuthentication.authenticate,expenseController.deleteExpense);
 
 module.exports = router;

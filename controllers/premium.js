b4d4fe139312ddae872c90427ea01da8bf9a27b6
Devina <44644511+Devina6@users.noreply.github.com>
2023-07-15 22:26:20 +0530

    Update premium.js

--- controllers/premium.js
+++ controllers/premium.js
@@ -8,17 +8,10 @@ const User = require('../models/user');
 const Expense = require('../models/expense');
 
 exports.leaderBoardStatus = (req,res,next) => {
-    console.log("in leader Board Status")
-    Expense.findAll({
-        attributes: ['user.id',
-          [sequelize.literal('user.firstName'), 'firstName'],
-          [sequelize.literal('user.lastName'), 'lastName'],
-          [sequelize.fn('SUM', sequelize.col('amount')), 'total_expense']
-        ],
-        include: [{model: User,attributes: []}],
-        group: ['user.id', 'firstName', 'lastName'],
-        order: [[sequelize.literal('total_expense'), 'DESC']]
-      })
+     User.findAll({
+    attributes:['firstName','lastName','totalExpense'],
+    order:[['totalExpense','DESC']]
+  })
       .then(result => {
         return res.json(result);
       })

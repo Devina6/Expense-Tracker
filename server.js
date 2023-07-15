f33fcdf0eec44946be881128139f89d3e60cc1d5
Devina <44644511+Devina6@users.noreply.github.com>
2023-07-15 20:00:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const Order = require('./models/order');
 const userRoutes = require('./routes/user');
 const expenseRoutes = require('./routes/expense');
 const purchaseRoutes = require('./routes/purchase');
+const premiumRoutes = require('./routes/premium');
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
@@ -21,6 +22,7 @@ app.use(cors());
 app.use(userRoutes);
 app.use('/expense',expenseRoutes);
 app.use('/purchase',purchaseRoutes);
+app.use('/premium',premiumRoutes);
 
 User.hasMany(Expense);
 Expense.belongsTo(User);

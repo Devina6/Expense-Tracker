104bae6b943b0263a96d47ba11d24e26e1d9c239
Devina <44644511+Devina6@users.noreply.github.com>
2023-07-17 19:57:00 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const userController = require('../controllers/user');
 
 router.post('/adduser',userController.postUser);
-router.post('/login',userController.login)
+router.post('/login',userController.login);
+router.post('/forgotpassword',userController.forgotPassword);
 
 module.exports = router;

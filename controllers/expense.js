ac9419a712c4dc5f83a6dbc418ff5c4fb14bb6a0
Devina <44644511+Devina6@users.noreply.github.com>
2023-07-17 18:26:33 +0530

    Update expense.js

--- controllers/expense.js
+++ controllers/expense.js
@@ -25,47 +25,40 @@ exports.postExpense = (req,res,next) => {
     let description = req.body.description;
     let category = req.body.category;
     let id = req.user.id
-    if(amount!='' && description!='' && category!='' && id!=''){
-        Expense.create({
-            amount:amount,
-            description:description,
-            category:category,
-            userId:id
-        })
-        .then( async(result) => {
-            
-            const updateTotalExpense = async (userId, amount) => {
-                try {
-                  const user = await User.findByPk(userId); 
-                  if (user) {
-                    const updatedTotalExpense = parseInt(user.totalExpense) + parseInt(amount);
-                    await user.update({ totalExpense: updatedTotalExpense });
-                  }
-                } 
-                catch (error) {
-                  console.log(error);
-                }
-              };  
-            updateTotalExpense(id, amount);         
-            res.json(result);
-        })
-        .catch(err => console.log(err))
+    sequelize.transaction(async (t)=>{
+        try{
+            if(amount!='' && description!='' && category!='' && id!=''){
+                const expense = await Expense.create({
+                    amount:amount,
+                    description:description,
+                    category:category,
+                    userId:id
+                },
+                {transaction:t})
+            const updatedTotalExpense = parseInt(req.user.totalExpense) + parseInt(amount);
+            await User.update({ totalExpense: updatedTotalExpense },{where:{id:id},transaction:t})
+            res.json({expense:expense})
+        }
     }
+    catch(err){
+        await t.rollback();
+        console.log(err)
+    }
+})
 }
 
 exports.deleteExpense = (req,res,next) => {
-
-    const id = req.params.expenseId;
-    Expense.findByPk(id)
-        .then(expense => {
-            if(!expense){
-                console.log("expense not found");
-            }else{
-                return expense.destroy();
-            }
-        })
-        .then(result => {
-           res.redirect('/expense/index')
-        })
-        .catch(err => console.log(err))
+sequelize.transaction(async(t)=>{
+        try{
+            const expense = await Expense.findOne({where:{id:req.params.expenseId}})
+            const del = await Expense.destroy({where:{id:req.params.expenseId,userId:req.user.id}},{transaction:t})
+            const updatedTotalExpense = parseInt(req.user.totalExpense) - parseInt(expense.amount);
+            await User.update({ totalExpense: updatedTotalExpense },{where:{id:req.user.id},transaction:t})
+            res.redirect('/expense/index')
+        }
+        catch(err){
+            await t.rollback()
+            return res.status(500).json({success:false,error:err})
+                }
+    })
 }

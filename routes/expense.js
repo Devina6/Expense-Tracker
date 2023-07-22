47e6f5983573adeec6bdc08afacbdb908795f31a
Devina <44644511+Devina6@users.noreply.github.com>
2023-07-22 22:27:33 +0530

    Update expense.js

--- routes/expense.js
+++ routes/expense.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const expenseController = require('../controllers/expense');
-const userAuthentication = require('../middleware/auth');
+const authentication = require('../middleware/auth');
 
 
-router.get('/index',userAuthentication.authenticate,expenseController.getIndex);
-router.post('/addExpense',userAuthentication.authenticate,expenseController.postExpense);
-router.get('/delete/:expenseId',userAuthentication.authenticate,expenseController.deleteExpense);
+router.get('/index',authentication.userAuthenticate,expenseController.getIndex);
+router.post('/addExpense',authentication.userAuthenticate,expenseController.postExpense);
+router.get('/delete/:expenseId',authentication.userAuthenticate,expenseController.deleteExpense);
 
 module.exports = router;

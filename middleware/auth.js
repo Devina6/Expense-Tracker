f6cf702ce24f302797105ad2e25eb6e6e5f59677
Devina <44644511+Devina6@users.noreply.github.com>
2023-07-22 22:25:32 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,9 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
-
-exports.authenticate = (req,res,next) => {
+const Password = require('../models/forgotpasswordrequests');
+exports.userAuthenticate = (req,res,next) => {
     try{
-        const token = req.header("Authorizaton");
+        const token = req.header("userAuthorization");
         const user = jwt.verify(token,'8ytrdfghbvfde34567ytdcvyr57465rtfgjf47gy4557tyfghchgtue4348768fdchgyr5437097ttrfchvr5676865343wdghjf5xdy46tcrs7re4ech6u53tdytr56ehgu');
         User.findByPk(user.userId)
             .then(user => {
@@ -13,7 +13,22 @@ exports.authenticate = (req,res,next) => {
             .catch(err => console.log(err))
     }catch(err){
         console.log(err);
-        return res.status(401).json({sucess:false})
+        return res.status(401).json({userSuccess:false})
     }
 }
 
+exports.passwordAuthenticate = (req,res,next) => {
+    try{
+        const token = req.header("passwordAuthorization");
+        const password = jwt.verify(token,'8ytrdfghbvfde34567ytdcvyr57465rtfgjf47gy4557tyfghchgtue4348768fdchgyr5437097ttrfchvr5676865343wdghjf5xdy46tcrs7re4ech6u53tdytr56ehgu');
+        Password.findByPk(password.userId.id)
+            .then(password =>{
+                req.password = password;
+                next();
+            })
+            .catch(err => console.log(err))
+    }catch(err){
+        console.log(err);
+        return res.status(401).json({passwordSuccess:false})
+    }
+}

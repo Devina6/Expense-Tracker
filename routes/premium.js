e4b4e892ac80140d513e6cfa33e56df8daf9e4dd
Devina <44644511+Devina6@users.noreply.github.com>
2023-07-22 22:27:51 +0530

    Update premium.js

--- routes/premium.js
+++ routes/premium.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const premiumController = require('../controllers/premium');
-const userAuthentication = require('../middleware/auth');
+
 
 
 router.get('/leaderboardstatus',premiumController.leaderBoardStatus);

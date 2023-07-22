b76818defbeb56e72f98b8af18411c81f73c7bb9
Devina <44644511+Devina6@users.noreply.github.com>
2023-07-22 22:24:50 +0530

    Update purchase.js

--- controllers/purchase.js
+++ controllers/purchase.js
@@ -2,6 +2,8 @@ const Razorpay = require('razorpay');
 const Order = require('../models/order');
 require('dotenv').config();
 
+const User = require('../models/user');
+
 exports.isPremium = (req,res,next) => {
     const user_id = req.user.id;
     User.findOne({where:{id:user_id}})
@@ -51,16 +53,14 @@ exports.updateTransactionStatus = async(req,res,next) => {
         const order = await Order.findOne({where:{orderId:order_id}})
         if(payment_id!="00000000"){
             const promise1 =  order.update({paymentId:payment_id,status:"SUCCESSFUL"})
-            const promise2 = req.user.update({ispremiumuser:true})
+            const promise2 = req.user.update({ispreminumuser:1})
             Promise.all([promise1,promise2])
                 .then(( )=> {
                     return res.status(202).json({success:true,message:"Transaction Successful"})
                 })
                 .catch(err => console.log(err))
         }else{
-            const promise1 =  order.update({paymentId:payment_id,status:"FAILURE"})
-            const promise2 = req.user.update({ispremiumuser:false})
-            Promise.all([promise1,promise2])
+            order.update({paymentId:payment_id,status:"FAILURE"})
                 .then(( )=> {
                     return res.status(202).json({success:false,message:"Transaction Failure"})
                 })

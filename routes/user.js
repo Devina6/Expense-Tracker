51423838595fe64107700160f011daba6baee60e
Devina <44644511+Devina6@users.noreply.github.com>
2023-08-28 19:23:50 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -3,13 +3,12 @@ const router = express.Router();
 
 const userController = require('../controllers/user');
 const authentication = require('../middleware/auth');
-
+const expenseController = require('../controllers/expense');
 
 router.post('/adduser',userController.signup);
 router.post('/login',userController.login);
 router.post('/forgotpassword',userController.forgotPassword);
 router.get('/password/:passwordId',userController.resetPassword);
 router.post('/resetpassword',authentication.userAuthenticate,authentication.passwordAuthenticate,userController.passwordReset);
-
 
 module.exports = router;

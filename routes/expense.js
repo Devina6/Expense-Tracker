7e3bd74193e3f308df5c7833d4d53ed2679de53d
Devina <44644511+Devina6@users.noreply.github.com>
2023-10-01 20:49:23 +0530

    dynamic pagination

--- routes/expense.js
+++ routes/expense.js
@@ -5,7 +5,7 @@ const expenseController = require('../controllers/expense');
 const authentication = require('../middleware/auth');
 
 
-router.get('/index',authentication.userAuthenticate,expenseController.getIndex);
+router.post('/index',authentication.userAuthenticate,expenseController.getIndex);
 router.post('/addExpense',authentication.userAuthenticate,expenseController.postExpense);
 router.get('/delete/:expenseId',authentication.userAuthenticate,expenseController.deleteExpense);
 

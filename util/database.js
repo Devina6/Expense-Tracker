a5dde48485ca740345e6015474c4406f53541bf2
Devina <44644511+Devina6@users.noreply.github.com>
2023-11-06 18:51:11 +0530

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,7 +1,8 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('expensetracker','root','SQLpassword',{
-    dialect:'mysql',
-    host:'localhost'
+require('dotenv').config();
+const sequelize = new Sequelize(process.env.DATABASE_SCHEME,process.env.DATABASE_USERNAME,process.env.DATABASE_PASSWORD,{
+    dialect:process.env.DATABASE_TYPE,
+    host:process.env.DATABASE_HOST
 });
 
 module.exports = sequelize;

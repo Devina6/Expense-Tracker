b7a48ca2e7b1d2e15e583b2326a8444229d9d566
Devina <44644511+Devina6@users.noreply.github.com>
2023-12-22 14:59:58 +0530

    ready for deployment

--- public/js/password.js
+++ public/js/password.js
@@ -3,7 +3,7 @@ async function resetPassword(e){
     const userToken = localStorage.getItem("userToken");
     const passwordToken = localStorage.getItem("passwordToken");
     try{
-        let page  = await axios.post('https://localhost:5000/password',{headers:{"userAuthorization":userToken,"passwordAuthorization":passwordToken}});
+        let page  = await axios.post('/password',{headers:{"userAuthorization":userToken,"passwordAuthorization":passwordToken}});
     }
     catch(err){
         console.log(err)
@@ -25,7 +25,7 @@ async function passwordreset(e){
                 password:newpassword,
             }
 
-            let reset = await axios.post(`http://localhost:5000/resetpassword`,obj,{headers:{"userAuthorization":userToken,"passwordAuthorization":passwordToken}})
+            let reset = await axios.post(`/resetpassword`,obj,{headers:{"userAuthorization":userToken,"passwordAuthorization":passwordToken}})
             console.log(reset)
         }else{
             

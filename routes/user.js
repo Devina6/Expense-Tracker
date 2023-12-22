4c2c69412d6363a99d7f6f29a908dcf444af906f
Devina <44644511+Devina6@users.noreply.github.com>
2023-12-22 15:13:11 +0530

    ready for deployment

--- routes/user.js
+++ routes/user.js
@@ -5,9 +5,17 @@ const userController = require('../controllers/user');
 const authentication = require('../middleware/auth');
 const expenseController = require('../controllers/expense');
 
-router.post('/adduser',userController.signup);
-router.post('/login',userController.login);
-router.post('/forgotpassword',userController.forgotPassword);
+router.get('/home',userController.gethomePage);
+router.get('',userController.geterrorPage);
+
+router.get('/signup',userController.getsignup);
+router.post('/adduser',userController.postsignup);
+
+router.get('/login',userController.getlogin)
+router.post('/login',userController.postlogin);
+
+router.get('/forgotpassword',userController.getforgotpassword);
+router.post('/forgotpassword',userController.postforgotPassword);
 router.get('/password/:passwordId',userController.resetPassword);
 router.post('/resetpassword',authentication.userAuthenticate,authentication.passwordAuthenticate,userController.passwordReset);
 
